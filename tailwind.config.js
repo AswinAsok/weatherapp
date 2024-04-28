d08834ae3b2b382cd570b640ca0da9c1baf521a3
Aswin Asok <aswinasokofficial@gmail.com>
2024-04-28 17:18:58 +0530

    feat(tailwind):configuring-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'weather-primary': '#00668A',
+        'weather-secondary': '#00A7E1'
+      }
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif']
+    },
+    container: {
+      padding: '2rem',
+      center: true
+    },
+    screens: {
+      sm: '640px',
+      md: '768px'
+    }
   },
   plugins: []
 }
